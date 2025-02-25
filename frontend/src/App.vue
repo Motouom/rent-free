dc2fd0abc07674ffdff934297d2547fc7bbbdcf7
Motouom <motouomvictor@gmail.com>
2025-02-25 18:25:24 +0100

    Implement registration page

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import RegistrationView from "@/views/RegistrationView.vue";
+</script>
+
+<template>
+  <div id="app">
+    <nav>
+      <RegistrationView></RegistrationView>
+    </nav>
+  </div>
+</template>
